ffddcb4b3b3ab461577793ff2c7a9bbfeab0fcd1
Matibidicaiphus <matibidicaiphus@gmail.com>
2022-09-13 12:57:16 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,13 +8,13 @@
  */
 int main(void)
 {
-  int a, b;
+	int a, b;
 
-  for (a = 1; a < 1024; a++)
-    {
-      if ((a % 3) == 0 || (a % 5) == 0)
-	b += a;
-    }
-  printf("%d\n", b);
-  return (0);
+	for (a = 1; a < 1024; a++)
+	{
+		if ((a % 3) == 0 || (a % 5) == 0)
+			b += a;
+	}
+	printf("%d\n", b);
+	return (0);
 }
